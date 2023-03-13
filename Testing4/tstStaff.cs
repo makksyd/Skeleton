cf02327c24e3ba7eb4b1b4e429b7997e5642d6a6
Maksymilian Zalewski <P2680114@my365.dmu.ac.uk>
2023-03-13 11:37:39 +0000

    Added Staff class testing

---
+++ Testing4/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //Test to see if that exists
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
